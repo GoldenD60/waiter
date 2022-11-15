6715b6d5febe4aa8caa61e54d62d7eba4578c2ae
David Skillman <65703529+GoldenD60@users.noreply.github.com>
2022-11-15 19:45:02 +0000

    Remove oranges from menu
    Stopped working because of stack.reverse(), for some reason it makes the entire stack type set to "None"

--- waiter.py
+++ waiter.py
@@ -11,8 +11,8 @@ import sys
 
 listed = [[]]
 
-#               I/O        I/O        INPUT     OUTPUT   OUTPUT    PUSH     PUSH      POP     POP     IF    IS    ISN'T      ADD        ADD       MINUS   MINUS     DIVIDE      DIVIDE      MULTIPLY     MULTIPLY    DUPE      DUPE      SWAP       SWAP     NULL    NULL      MOD      MOD      INFINITE   NO POP I/O NO POP I/O
-dictionary = ["burger", "burgers", "coleslaw", "chips", "fries", "waters", "water", "salts", "salt", "if", "is", "isn't", "chicken", "chickens", "beef", "beefs", "mushroom", "mushrooms", "stuffings", "stuffing", "lemon", "lemons", "orange", "oranges", "plum", "plums", "crisp", "crisps", "infinite", "ketchup", "ketchups"]
+#               I/O        I/O        INPUT     OUTPUT   OUTPUT    PUSH     PUSH      POP     POP     IF    IS    ISN'T      ADD        ADD       MINUS   MINUS     DIVIDE      DIVIDE      MULTIPLY     MULTIPLY    DUPE      DUPE     NULL    NULL      MOD      MOD      INFINITE   NO POP I/O NO POP I/O
+dictionary = ["burger", "burgers", "coleslaw", "chips", "fries", "waters", "water", "salts", "salt", "if", "is", "isn't", "chicken", "chickens", "beef", "beefs", "mushroom", "mushrooms", "stuffings", "stuffing", "lemon", "lemons", "plum", "plums", "crisp", "crisps", "infinite", "ketchup", "ketchups"]
 punctuation = [" ", ",", ".", ":", ";"]
 
 def lex(text):
@@ -73,9 +73,6 @@ def parse(text):
 				stack.insert(0, stack[0] % stack[1])
 				stack.pop(1)
 				stack.pop(1)
-			if listed[i][j] in ["orange", "oranges"]:
-				for k in range(int(listed[i][j-1])):
-					stack = stack.reverse()
 			if listed[i][j] == "if":
 				if listed[i][j+1] == "is":
 					if str(stack[0]) == listed[i][j+2]:
